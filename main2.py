06eafcce351caf38ab41a6a88f5cd6e4cef88c0e
NevLax <qwegornikitin@gmail.com>
2025-02-16 21:13:55 +0400

    simple update

--- main2.py
+++ main2.py
@@ -1,16 +1,16 @@
 import asyncio
 from websockets.asyncio.client import connect
-
+messHandle = "MESSAGE\n"
 
 async def hello():
     async with connect("ws://localhost:8080/live") as websocket:
         await websocket.send("STREAMER nlx")
         await asyncio.sleep(1)
-        await websocket.send("code")
+        await websocket.send(messHandle + "code")
         await asyncio.sleep(3)
-        await websocket.send("microcode")
+        await websocket.send(messHandle + "microcode")
         await asyncio.sleep(2)
-        await websocket.send("GIGA CODE")
+        await websocket.send(messHandle + "GIGA CODE")
 
 
 if __name__ == "__main__":
